feb1a4b7b6c6e6e9e5fb0b243abaec66344d7991
amangusss <amanaman2004@gmail.com>
2024-10-17 10:18:36 +0600

    Initial commit

---
+++ src/main/java/alatoo/edu/kg/lowkeystudentsapi/LowKeyStudentsApiApplication.java
@@ -0,0 +1,13 @@
+package alatoo.edu.kg.lowkeystudentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowKeyStudentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowKeyStudentsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alatoo/edu/kg/lowkeystudentsapi/LowKeyStudentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.edu.kg.lowkeystudentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowKeyStudentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
