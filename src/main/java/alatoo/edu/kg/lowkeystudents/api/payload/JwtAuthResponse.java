cac9d1f7712e6f85cba9d45504b385cb84756214
amangusss <amanaman2004@gmail.com>
2024-11-27 04:42:59 +0600

    implemented spring security and configured it

---
+++ src/main/java/alatoo/edu/kg/lowkeystudents/api/payload/JwtAuthResponse.java
@@ -0,0 +1,13 @@
+package alatoo.edu.kg.lowkeystudents.api.payload;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
---
+++ src/main/java/alatoo/edu/kg/lowkeystudents/api/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package alatoo.edu.kg.lowkeystudents.api.service.impl;
+
+import alatoo.edu.kg.lowkeystudents.api.exceptions.NotFoundException;
+import alatoo.edu.kg.lowkeystudents.store.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return repository.findByUsername(login)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found by: " + login));
+    }
+}
