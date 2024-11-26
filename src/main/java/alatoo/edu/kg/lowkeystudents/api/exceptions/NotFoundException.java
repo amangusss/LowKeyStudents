ab8e97f3cde9cb0275a3395c31512a49c16ca040
amangusss <amanaman2004@gmail.com>
2024-11-27 04:43:27 +0600

    added exceptions in project

---
+++ src/main/java/alatoo/edu/kg/lowkeystudents/api/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package alatoo.edu.kg.lowkeystudents.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends BaseException {
+    public NotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
+    }
+}
