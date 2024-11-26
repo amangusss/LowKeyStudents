08a4efe12b264ef85dc2ac734ddf11429c5a77c3
amangusss <amanaman2004@gmail.com>
2024-11-27 04:47:06 +0600

    added main files to project

--- src/main/java/alatoo/edu/kg/lowkeystudents/LowKeyStudentsApiApplication.java
+++ src/main/java/alatoo/edu/kg/lowkeystudents/LowKeyStudentsApiApplication.java
@@ -1,8 +1,10 @@
-package alatoo.edu.kg.lowkeystudents_api;
+package alatoo.edu.kg.lowkeystudents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LowKeyStudentsApiApplication {
 
--- src/test/java/alatoo/edu/kg/lowkeystudents/LowKeyStudentsApiApplicationTests.java
+++ src/test/java/alatoo/edu/kg/lowkeystudents/LowKeyStudentsApiApplicationTests.java
@@ -1,4 +1,4 @@
-package alatoo.edu.kg.lowkeystudents_api;
+package alatoo.edu.kg.lowkeystudents;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
